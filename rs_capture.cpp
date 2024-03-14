5d88e1818d164c3403cdeb766825b9bce4aa5ade
Anderson A.C. Alves <60412185+alvesand@users.noreply.github.com>
2024-03-13 20:06:39 -0400

    new running time options for the config file

--- rs_capture.cpp
+++ rs_capture.cpp
@@ -65,7 +65,7 @@ float box_dist(const rs2::depth_frame& depth_frame, int x_min, int x_max, int y_
 void readParFile(const string& filename, vector<string*>& pointers)
 {
 
-	ifstream file(filename);
+	std::ifstream file(filename.c_str());
 	string line;
 
 	if(!file.is_open())
@@ -131,8 +131,6 @@ void write_log(std::string type, std::string message, int verbose, const std::st
  * @return new directory in the current working directory
  */
 
-
-
 void create_dir(const string& directoryName, int verbose, string path)
 {
 	
@@ -145,34 +143,58 @@ void create_dir(const string& directoryName, int verbose, string path)
 }
 
 int main() {
-	
+	string fileName;
+	cout << "Please type the parameter file path: " << endl;
+	getline(cin, fileName);//= "/home/alveslab/rs_capture/input.par";
 	//Getting the starting working directory where the log file will be saved
 		
 	char buffer[50];
 	getcwd(buffer, sizeof(buffer));
-	string startDirectory = std::string(buffer);	
+	string startDirectory = std::string(buffer);
+	string final_dir;
+	std::ostringstream oss;		
 	
 	string msg;
-	string target_directory, cam_name, W, H, fps, distance, x_min, x_max, y_min, y_max, rgbAE, laserIR, allowRGB, verbose1, dev_num;
-	vector<string*> ptrs = {&target_directory, &cam_name, &W, &H, &fps, &distance, &x_min, &x_max, &y_min, &y_max, &rgbAE, &laserIR, &allowRGB, &verbose1, &dev_num};
-	const string fileName = "input.par";
-	readParFile (fileName, ptrs);	
+	string target_directory, cam_name, W, H, fps, distance, x_min, x_max, y_min, y_max, rgbAE, laserIR, allowRGB, verbose1, dev_num, run_length;
+	vector<string*> ptrs = {&target_directory, &cam_name, &W, &H, &fps, &distance, &x_min, &x_max, &y_min, &y_max, &rgbAE, &laserIR, &allowRGB, &verbose1, &dev_num, &run_length};
+	
+	readParFile (fileName, ptrs);
+	int verbose = stoi(verbose1);
+	int dev_choice = stoi(dev_num);
+	int numAtpp = 4;
+	int currentAtpp = 0;
+	auto duration = std::chrono::minutes(stoi(run_length));
+	
 	vector <string> parNames = {"Target Directory: ", "Camera Name: ", "Image Width: ",
 				"Image Height: ", "Frames per Second: ", "Distance from ROI: ",
  				"x_min: ", "x_max: ", "y_min: ", "y_max: ",
  				"Auto Exposure Priority for RGB (0 = No, 1 = Yes): ",
  				"Allow IR laser (0 = No, 1 = Yes): ",
- 				"Allow RGB capture (0 = No, 1 = Yes): ", "verbose: ", "Device Number: "};
-	int verbose = stoi(verbose1);
-	int dev_choice = stoi(dev_num);
-	int numAtpp = 4;
-	int currentAtpp = 0;			
+ 				"Allow RGB capture (0 = No, 1 = Yes): ", "verbose: ", "Device Number: ", "Time to run (minutes): "};
+	
+	if (chdir(target_directory.c_str())==0){
+		
+		final_dir = target_directory;		
+		oss.str("");
+		oss << 	"Working directory set to " << target_directory;
+		write_log("INFO",oss.str() , verbose, final_dir); 
+	}else{
+		final_dir = startDirectory;	
+		oss.str("");
+		write_log("ERROR", "Failed to set directory. Reason: directory does not exist!!", verbose, target_directory);	
+		oss << "Using " << buffer << " as working directory";
+		write_log("WARNING", oss.str(), verbose, final_dir);	
+		target_directory = buffer;
+	}
+	
+	
+	if (run_length == "-1"){duration = std::chrono::minutes(10);}					
 
-	write_log("INFO", "####---List of parameters configured ---###", verbose, startDirectory);	
+	write_log("INFO", "####---List of parameters configured ---###", verbose, final_dir);	
 	for (int i = 0; i < parNames.size(); i++)
 	{
 	  msg = parNames[i] + " " + *ptrs[i];
-	  write_log("INFO", msg, verbose, startDirectory);
+	  write_log("INFO", msg, verbose, final_dir);
 	}
 	cout << endl;
 	
@@ -182,14 +204,14 @@ int main() {
 	auto devices = ctx.query_devices();
 	if (devices.size() == 0)
 	{
-		write_log("ERROR", "No realsense device connected, please check USB connectivity", verbose, startDirectory);
+		write_log("ERROR", "No realsense device connected, please check USB connectivity", verbose, final_dir);
 		return 1; 
 	}
 	else
 	{
 		std::ostringstream out;
 		out << "Number of rs devices connected: " << devices.size();		
-		write_log("INFO",  out.str(), verbose, startDirectory);
+		write_log("INFO",  out.str(), verbose, final_dir);
 	}
 
 	
@@ -206,16 +228,16 @@ int main() {
 		{
 			for (auto i = 0; i < 30; i++) piptest.wait_for_frames();
 			//If sucess continue pipeline
-			write_log("INFO", "Frame streaming estabilhed, ready to start...", verbose, startDirectory);
+			write_log("INFO", "Frame streaming estabilhed, ready to start...", verbose, final_dir);
 			piptest.stop(); //stop the current test pipeline					
 			break;
 		}catch(const rs2::error& e)
 		{
 			std::stringstream error_text;
 			error_text << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what();
-			write_log("ERROR", error_text.str(), verbose, startDirectory);
+			write_log("ERROR", error_text.str(), verbose, final_dir);
 			cout << endl;
-			write_log("INFO", "Trying to restart camera ... ", verbose, startDirectory);
+			write_log("INFO", "Trying to restart camera ... ", verbose, final_dir);
 			std::this_thread::sleep_for(std::chrono::seconds(1)); // Wait before reattempting
 			dev.hardware_reset();
 			std::this_thread::sleep_for(std::chrono::seconds(1)); // Wait before reattempting
@@ -224,7 +246,7 @@ int main() {
 			if(currentAtpp==numAtpp)
 			{
 			piptest.stop();
-			write_log("ERROR", "unable to get frames, please check USB connectivity", verbose, startDirectory);
+			write_log("ERROR", "unable to get frames, please check USB connectivity", verbose, final_dir);
 			return 1;
 			}
 		}		
@@ -256,67 +278,59 @@ int main() {
 	depth_sensor.set_option(RS2_OPTION_EMITTER_ENABLED, stoi(laserIR));
 	
 	time_t start = time(nullptr);
-	std::ostringstream oss;	
-    	oss << "Camera triggered at: " << ctime(&start);	
-    	write_log("INFO", oss.str(), verbose, startDirectory);	
+	oss << "Camera triggered at: " << ctime(&start);	
+    	write_log("INFO", oss.str(), verbose, final_dir);	
 	
-	if (chdir(target_directory.c_str())==0){
-	oss.str("");
-	oss << 	"Working directory set to " << target_directory;
-	write_log("INFO",oss.str() , verbose, startDirectory); 
-	}else{
-	//char buffer[50];
-        //getcwd(buffer, sizeof(buffer));
-	oss.str("");
-	write_log("ERROR", "Failed to set directory. Reason: directory does not exist!!", verbose, startDirectory);	
-	oss << "Using " << buffer << " as working directory";
-	write_log("WARNING", oss.str(), verbose, startDirectory);	
-	target_directory = buffer;
-	}
+	
 
 	//Create a directory named by date to store the images		
 	string date = get_current_datetime();
 	date = date.substr(0, 8);
     	string directoryName = cam_name+"_"+date;	
-	create_dir(directoryName, verbose, startDirectory);    	
+	create_dir(directoryName, verbose, final_dir);    	
 	//
-	string current_dir = target_directory+"/"+directoryName;
-	write_log("INFO", "Images will be saved at: " + current_dir, verbose, startDirectory);	
+	string current_dir = final_dir+"/"+directoryName;
+	write_log("INFO", "Images will be saved at: " + current_dir, verbose, final_dir);	
 	
 	//Creating folders to store data	
 	
 	//Binary Depth Directory
 	string depthdir = current_dir+"/depth_bin";
-	create_dir(depthdir, verbose, startDirectory);
+	create_dir(depthdir, verbose, final_dir);
 	
 	//Color frames directory (if allowed)	
 	string colordir = current_dir+"/rgb_png";
 	if (allowRGB=="1")
 	{
-	create_dir(colordir, verbose, startDirectory);
+	create_dir(colordir, verbose, final_dir);
 	}
 
 	//Colored depth directory
 	string col_depthdir = current_dir+"/depth_png";
-	create_dir(col_depthdir, verbose, startDirectory);
+	create_dir(col_depthdir, verbose, final_dir);
 
 	//IR frames directory
 	string irdir = current_dir+"/IR_png";
-	create_dir(irdir, verbose, startDirectory);	
+	create_dir(irdir, verbose, final_dir);	
+	
+	auto begin = std::chrono::steady_clock::now();		
+	int frame_count = 0;
 
 	//BEGIN MAIN LOOP 
-	while (true)
-	{
+	while (std::chrono::steady_clock::now() - begin < duration)
+	{		
+	  if (run_length == "-1"){begin = chrono::steady_clock::now() + std::chrono::minutes(10);}//Runs the camera for indefinite time	
 	  try{
 		rs2::frameset frame = pipe.wait_for_frames();
 	  	auto dframe = frame.get_depth_frame();
 	  	//float dist = dframe.get_distance(dframe.get_width() / 2, dframe.get_height() / 2);
 	 	 float dist = box_dist(dframe, stoi(x_min),stoi(x_max),stoi(y_min),stoi(y_max));  
 	  	//cout << dist << " m" << endl;
-	 	 int frame_count = 0;	  	            
+	 	 	  	            
 	 	 if (dist < stof(distance))
        		 {
-			write_log("INFO", "Average distance from ROI is " + std::to_string(dist) + " m", verbose, startDirectory);  		
+			frame_count++; // Increment the frame count
+			write_log("INFO", "Average distance from ROI is " + std::to_string(dist) + " m", verbose, final_dir);  		
 	    		string time_now = get_current_datetime();
 
 	    		if (frame && frame.get_data())
@@ -325,7 +339,7 @@ int main() {
 	    	    
 	        	string depthFilename = depthdir+"/"+time_now+"_"+to_string(frame_count)+"_d.bin";    
                   	ofstream(depthFilename, ios::binary).write((char*)dframe.get_data(), dframe.get_width() * dframe.get_height()*dframe.get_bytes_per_pixel());
-			write_log("INFO", "Saved " + depthFilename, verbose, startDirectory);	
+			write_log("INFO", "Saved " + depthFilename, verbose, final_dir);	
 	       
 		  	// Save the color image
 			if (allowRGB=="1")
@@ -333,37 +347,43 @@ int main() {
 	      	  	auto cframe = frame.get_color_frame();	    
 	      	  	string colorFilename = colordir+"/"+time_now+"_"+"c"+".png";            
               	  	stbi_write_png(colorFilename.c_str(), cframe.get_width(), cframe.get_height(),cframe.get_bytes_per_pixel(), cframe.get_data(), cframe.get_stride_in_bytes());            
-              	  	write_log("INFO", "Saved " + colorFilename, verbose, startDirectory);		
+              	  	write_log("INFO", "Saved " + colorFilename, verbose, final_dir);		
 	      	 	}
 		
 	         	// Save IR image
 	         	auto iframe = frame.get_infrared_frame();	    
 	         	string iFilename = irdir+"/"+time_now+"_"+"i"+".png";            
                  	stbi_write_png(iFilename.c_str(), iframe.get_width(), iframe.get_height(),iframe.get_bytes_per_pixel(), iframe.get_data(), iframe.get_stride_in_bytes());
-	         	write_log("INFO", "Saved " + iFilename, verbose, startDirectory);           
+	         	write_log("INFO", "Saved " + iFilename, verbose, final_dir);           
               	
 	         	// Save the colored depth image as png
 	         	// Colorize the depth frame
                  	rs2::video_frame d_color = color_map.colorize(dframe);	    
 	         	string col_depthFilename = col_depthdir+"/depth_"+time_now+"_"+"d"+".png";
 	         	stbi_write_png(col_depthFilename.c_str(), d_color.get_width(), d_color.get_height(), d_color.get_bytes_per_pixel(), d_color.get_data(),  d_color.get_stride_in_bytes());             
-                 	write_log("INFO", "Saved " + col_depthFilename, verbose, startDirectory);       	    
+                 	write_log("INFO", "Saved " + col_depthFilename, verbose, final_dir);       	    
             
-              		frame_count++; // Increment the frame count
+              		
 	      		}	    
 	   	}
          }catch (const rs2::error& e)	{
 		std::stringstream error_text;
 		error_text << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what();
 		//cout << error_text.str() << endl;
-		write_log("ERROR", error_text.str(), verbose, startDirectory);
-		pipe.stop();
-		return 1;
+		write_log("ERROR", error_text.str(), verbose, final_dir);
+		write_log("INFO", "Trying to restart camera ... ", verbose, final_dir);
+		std::this_thread::sleep_for(std::chrono::seconds(1)); // Wait before reattempting
+		dev.hardware_reset();
+		std::this_thread::sleep_for(std::chrono::seconds(1)); // Wait before reattempting		
 	}	
     
        }
 	pipe.stop();
-	  
+	
+	std::stringstream final_msg;
+	final_msg <<  "Exiting application with no errors.";
+	write_log("INFO", final_msg.str() , verbose, final_dir);
+	
 	return 0;
 }
 
